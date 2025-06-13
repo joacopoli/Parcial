b8a239cc13b60dc7075b3d52a901f7130288c650
joacopoli <143116412+joacopoli@users.noreply.github.com>
2025-06-13 18:15:54 -0300

    Add files via upload

---
+++ Insert1.sql
@@ -0,0 +1,6 @@
+--INSERT INTO usuarios (nombre, apellido, Email, PasswordHash, Nivel)
+--VALUES ('Juan', 'Perez', 'prueba@ejemplo.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 0);
+
+INSERT INTO usuarios (nombre, apellido, Email, PasswordHash, Nivel)
+VALUES ('Joaquin', 'Poli', 'joaco@ejemplo.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1);
+select * from usuarios
